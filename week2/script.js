3df52b858d8e0f3306f39624eb02958f8c4e1596
grac-ies <106109485+grac-ies@users.noreply.github.com>
2024-01-29 15:48:51 -0700

    Add files via upload

---
+++ week2/script.js
@@ -0,0 +1,11 @@
+console.log("1.https://cursoreffects.com");
+console.log("2.https://www.nytimes.com/games/wordle/index.html");
+console.log("3.https://theuselessweb.com");
+
+console.log("1.animal crossing");
+console.log("2.stardew valley");
+console.log("3.fortnite");
+
+console.log("1. Meagan Thee Stallion, shes a female rapper and is known for empowering women");
+console.log("2. grimes, an artist who bends and twists the rules of music and style");
+console.log("3. marina abramović, her art explores the human mind and its limits making people think.");
